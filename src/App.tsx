dbd730164105e03dbc32cb29c10d1e87b124be7f
Christian <christianshenmue@hotmail.com>
2021-06-25 20:45:00 -0300

    Aulas 4+5

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { AdminRoom } from "./pages/AdminRoom";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
+          <Route path="/rooms/:id" component={Room} />,
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
